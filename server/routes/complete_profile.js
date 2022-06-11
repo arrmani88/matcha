af157d90bd759e1a6c9be1cada73af12b7c53c64
Anas El boudali <anel-bou@e3r6p9.1337.ma>
2022-06-11 12:27:17 +0100

    lock

--- server/routes/complete_profile.js
+++ server/routes/complete_profile.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const dbController = require('../models/db_controller')
 const validateToken = require('../middlewares/validate_token')
 const { isBirthday, isGender } = require('../functions/input_validation')
+const { compareSync } = require('bcrypt')
 const fieldIsNullMessage = "One of the fields 'birthday', 'gender', 'sexualPreferences', 'biography' or 'tags' is empty or wasn't sent"
 
 const validateProfileCompletionInput = (req, res, next) => {
@@ -26,52 +27,69 @@ router.post('/', validateToken, validateProfileCompletionInput, async (req, res)
 	const { birthday, gender, sexualPreferences, biography, tags } = req.body
 	try {
 		let existingTagsArray = []
+		let newTagsIds = []
 		let getExistingTagsQuery = "SELECT * FROM tags WHERE value in ("
-		
-		let insertNewTagsQuery = "INSERT INTO tags(value) VALUES("
+		let insertNewTagsQuery = "INSERT INTO tags(value) VALUES"
 		let count = 1;
 		for (const tag of tags) {
 			count != tags.length ? getExistingTagsQuery += ("'" + tag + "', ") : getExistingTagsQuery += ("'" + tag + "')")
 			count++
 		}
-		dbController.query(
-			getExistingTagsQuery,
-			(err, result) => {
-				if (err) return res.json({error: err}) 
-				else {
-					existingTagsArray = result
-					count = 1
-					let tagExists = false
-					for (let tag of tags) {
-						for (let existingTag of existingTagsArray) {
-							if (existingTag.value == tag) tagExists = true
+		await new Promise(() => {
+			dbController.query(
+				getExistingTagsQuery,
+				(err, result) => {
+					if (err) return res.json({ error: err.message }) 
+					else if ((tags.length - result.length) > 0) { // if there are some tags to add to the DB
+						console.log('444')
+						existingTagsArray = result
+						const newTagsLength = tags.length - existingTagsArray.length
+						let tagExists = false
+						let firstAddedTagId
+						count = 1
+						for (let tag of tags) {
+							for (let existingTag of existingTagsArray) if (existingTag.value == tag) tagExists = true
+							if (!tagExists) {
+								count != newTagsLength ? insertNewTagsQuery += ("('" + tag + "'), ") : insertNewTagsQuery += ("('" + tag + "')")
+								count++
+							}
+							tagExists = false
 						}
-						if (!tagExists) {
-							count != existingTagsArray.length ? insertNewTagsQuery += ("'" + tag + "', ") : insertNewTagsQuery += ("'" + tag + "')")
-							count++
-						}
-						tagExists = false
+						dbController.query(
+							insertNewTagsQuery,
+							(err, result) => {
+								console.log('333')
+								if (err) res.status(400).json({ error: err })
+								else {
+									firstAddedTagId = result.insertId
+									count = 0
+									while (count < newTagsLength) {
+										newTagsIds.push(firstAddedTagId + count)
+										count++
+									}
+									// console.log(newTagsIds)
+									return (console.log('1111111'))
+								}
+							}
+						)
 					}
-					console.log(insertNewTagsQuery)
-					dbController.query(
-						"UPDATE users SET " +
-							"birthday = ?, " +
-							"gender = ?, " +
-							"sexualPreferences = ?, " +
-							"biography = ? " +
-						"WHERE id = ?",
-						[birthday, gender, sexualPreferences, biography, req.user.id],
-						(err) => {
-							if (err) return res.json({error: err})
-							else return res.status(200).send('Profile completed successfully')
-						}
-					)
 				}
-			}
-		)
-
-
-		
+			)
+		})
+		console.log('2222')
+		// dbController.query(
+		// 	"UPDATE users SET " +
+		// 		"birthday = ?, " +
+		// 		"gender = ?, " +
+		// 		"sexualPreferences = ?, " +
+		// 		"biography = ? " +
+		// 		"WHERE id = ?",
+		// 	[birthday, gender, sexualPreferences, biography, req.user.id],
+		// 	(err) => {
+		// 		if (err) return res.json({error: err})
+		// 		else return res.status(200).send('Profile completed successfully')
+		// 	}
+		// )
 	} catch (err) {
 		console.log(err)
 		return res.json({ error: err })
