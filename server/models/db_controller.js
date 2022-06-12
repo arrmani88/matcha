3ea893363bd8cf0f714d3634c0c9428224086722
Anas El boudali <anel-bou@e3r8p9.1337.ma>
2022-06-12 19:58:01 +0100

    sending queries synchronously -- in progress

--- server/models/db_controller.js
+++ server/models/db_controller.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 
 try {
     const dbController = mysql.createConnection({
-        host: '192.168.99.114',
+        host: '192.168.99.117',
         user: 'root',
         password: 'root',
         database: 'matcha'
--- server/routes/complete_profile.js
+++ server/routes/complete_profile.js
@@ -7,27 +7,27 @@ const { compareSync } = require('bcrypt')
 const fieldIsNullMessage = "One of the fields 'birthday', 'gender', 'sexualPreferences', 'biography' or 'tags' is empty or wasn't sent"
 
 const validateProfileCompletionInput = (req, res, next) => {
-    const { birthday, gender, sexualPreferences, biography, tags } = req.body
-    if (!birthday || !gender || !sexualPreferences || !biography || !tags) {
-        return res.status(422).json({error: {'details': fieldIsNullMessage}})
-    } else if (!isBirthday(birthday)){
-		return res.status(422).json({error: {"details": "Invalid 'birthday' syntax, should be like YYYY-MM-DD"}})
-    } else if (!isGender(gender)){
-		return res.status(422).json({error: {"details": "Invalid 'gender' syntax, should be either 'M', 'F' or 'N' (if not specified)"}})
-    } else if (!isGender(sexualPreferences)){
-		return res.status(422).json({error: {"details": "Invalid 'sexualPreferences' syntax, should be either 'M', 'F' or 'N' (if not specified)"}})
-    } else if (tags.length != 5){
-		return res.status(422).json({error: {"details": "Invalid 'tags' field, should contain 5 tags"}})
-    } else {
-        next()
-    }
+	const { birthday, gender, sexualPreferences, biography, tags } = req.body
+	if (!birthday || !gender || !sexualPreferences || !biography || !tags) {
+		return res.status(422).json({ error: { 'details': fieldIsNullMessage } })
+	} else if (!isBirthday(birthday)) {
+		return res.status(422).json({ error: { "details": "Invalid 'birthday' syntax, should be like YYYY-MM-DD" } })
+	} else if (!isGender(gender)) {
+		return res.status(422).json({ error: { "details": "Invalid 'gender' syntax, should be either 'M', 'F' or 'N' (if not specified)" } })
+	} else if (!isGender(sexualPreferences)) {
+		return res.status(422).json({ error: { "details": "Invalid 'sexualPreferences' syntax, should be either 'M', 'F' or 'N' (if not specified)" } })
+	} else if (tags.length != 5) {
+		return res.status(422).json({ error: { "details": "Invalid 'tags' field, should contain 5 tags" } })
+	} else {
+		next()
+	}
 }
 
 router.post('/', validateToken, validateProfileCompletionInput, async (req, res) => {
 	const { birthday, gender, sexualPreferences, biography, tags } = req.body
 	try {
 		let usersTagsQuery
-		let tagsIds = []
+		let allTagsIds = []
 		let getExistingTagsQuery = "SELECT * FROM tags WHERE value in ("
 		let newTagsQuery = "INSERT INTO tags(value) VALUES"
 		let count = 1;
@@ -41,8 +41,9 @@ router.post('/', validateToken, validateProfileCompletionInput, async (req, res)
 				(err, result) => {
 					if (err) return rej(err)
 					else {
-						for (let existingTag of result) tagsIds.push(existingTag.id)
+						for (let existingTag of result) allTagsIds.push(existingTag.id)
 						if ((tags.length - result.length) > 0) { // if there are some new tags to add to the DB
+							console.log('><><><')
 							const newTagsLength = tags.length - result.length
 							let tagExists = false
 							let firstAddedTagId
@@ -58,46 +59,60 @@ router.post('/', validateToken, validateProfileCompletionInput, async (req, res)
 							dbController.query(
 								newTagsQuery,
 								(err, result) => {
-									console.log(newTagsQuery)
 									if (err) rej(err)
 									else {
 										firstAddedTagId = result.insertId
 										count = 0
 										while (count < newTagsLength) {
-											tagsIds.push(firstAddedTagId + count)
+											allTagsIds.push(firstAddedTagId + count)
 											count++
 										}
-										
 									}
 								}
 							)
 						}
+						console.log('1>>' + allTagsIds + '<<')
 					}
 				}
 			)
-			res('123')
+			res('')
 		})
-		await new Promise((res, rej) => {
-			dbController.query(
-				"INSERT INTO usersTags(uid, tagId) VALUES",
-				(err, result) => {
-					
-				}
-			)
-		})
-		dbController.query(
-			"UPDATE users SET " +
-				"birthday = ?, " +
-				"gender = ?, " +
-				"sexualPreferences = ?, " +
-				"biography = ? " +
-				"WHERE id = ?",
-			[birthday, gender, sexualPreferences, biography, req.user.id],
-			(err) => {
-				if (err) return res.json({error: err})
-				else return res.status(200).send('Profile completed successfully')
-			}
-		)
+		console.log('2>>' + allTagsIds + '<<')
+		
+
+		// await new Promise((res, rej) => {
+		// 	usersTagsQuery = "INSERT INTO usersTags(uid, tagId) VALUES"
+		// 	count = 1
+		// 	for (let tag of allTagsIds) {
+		// 		usersTagsQuery += (count < 5 ? "('" + tag + "')," : "('" + tag + "')")
+		// 		count++
+		// 	}
+		// 	console.log(usersTagsQuery)
+		// 	res('')
+			// dbController.query(
+			// 	"INSERT INTO usersTags(uid, tagId) VALUES",
+			// 	(err, result) => {
+
+			// 	}
+			// )
+		// })
+
+
+		// dbController.query(
+		// 	"UPDATE users SET " +
+		// 		"birthday = ?, " +
+		// 		"gender = ?, " +
+		// 		"sexualPreferences = ?, " +
+		// 		"biography = ? " +
+		// 		"WHERE id = ?",
+		// 	[birthday, gender, sexualPreferences, biography, req.user.id],
+		// 	(err) => {
+		// 		if (err) return res.json({error: err})
+		// 		else return res.status(200).send('Profile completed successfully')
+		// 	}
+		// )
+
+		res.send('finished')
 	} catch (err) {
 		console.log(err)
 		return res.json({ error: err })
