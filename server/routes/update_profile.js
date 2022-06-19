5aa9fb6d45c84ccc7215b48222677ad32263332c
Anas El boudali <anel-bou@student.1337.ma>
2022-06-19 15:31:51 +0100

    swp

--- server/routes/update_profile.js
+++ server/routes/update_profile.js
@@ -5,6 +5,13 @@ const validateToken = require('../middlewares/validate_token.js')
 const isAccountComplete = require('../middlewares/is_account_complete.js')
 const confirmIdentityWithPassword = require('../middlewares/confirm_identity_with_password.js')
 const util = require('util')
+const queryPromise = util.promisify(dbController.query.bind(dbController))
+var oldTagsIDs = []
+var newTagsIDs = []
+let getExistingTagsQuery = "SELECT * FROM tags WHERE value in ("
+let insertNewTagsQuery = "INSERT INTO tags(value) VALUES"
+let j
+let count = 1
 
 const getArrayOfUpdatedFields = (body, id) => {
 	const { newFirstname, newLastname, newUsername, newEmail, newPassword, newBirthday, newGender, newSexualPreferences, newBiography } = body
@@ -32,83 +39,71 @@ const getArrayOfUpdatedTags = (body, id) => {
 	return rtrn
 }
 
-router.post('/', validateToken, confirmIdentityWithPassword, isAccountComplete, async (req, res) => {
-	try {
-		const queryPromise = util.promisify(dbController.query.bind(dbController))
-		const { newFirstname, newLastname, newUsername, newEmail, newPassword, newBirthday, newGender, newSexualPreferences, newBiography, oldTags, newTags} = req.body
-		var oldTagsIDs = []
-		var newTagsIDs = [] 
-		let getExistingTagsQuery = "SELECT * FROM tags WHERE value in ("
-		let j
-		let count = 1
-		/* ************** GETTING OLD TAGS IDs **************** */
-		if (newTags != null && oldTags != null) {
+const getOldTagsIDs = async (oldTags) => {
+	var result = await queryPromise( // get old tags IDs
+		"SELECT * FROM tags WHERE " +
+			(oldTags.length >= 1 ?    "value = ?" : "") +
+			(oldTags.length >= 2 ? "or value = ?" : "") +
+			(oldTags.length >= 3 ? "or value = ?" : "") +
+			(oldTags.length >= 4 ? "or value = ?" : "") +
+			(oldTags.length >= 5 ? "or value = ?" : ""),
+		oldTags
+	)
+	for (let i = 0 ; i < oldTags.length ; i++)
+		for (j = 0 ; j < result.length ; j++)
+			if (result[j].value == oldTags[i])
+				oldTagsIDs.push(result[j].id)
+	console.log('old tags IDs:' + oldTagsIDs)
+}
 
-			var result = await queryPromise( // get old tags IDs
-				"SELECT * FROM tags WHERE " +
-					(oldTags.length >= 1 ?    "value = ?" : "") +
-					(oldTags.length >= 2 ? "or value = ?" : "") +
-					(oldTags.length >= 3 ? "or value = ?" : "") +
-					(oldTags.length >= 4 ? "or value = ?" : "") +
-					(oldTags.length >= 5 ? "or value = ?" : ""),
-				oldTags
-			)
-		}
-		console.log('<-------------------->')
-		for (let i = 0 ; i < oldTags.length ; i++) {
-			j = 0
-			for (; j < result.length ; j++) {
-				if (result[j].value == oldTags[i]) {
-					oldTagsIDs.push(result[j].id)
-				}
-			}
-		}
-		console.log(oldTagsIDs)
-		/****************************************************** */
-		/**************** GETTING NEW TAGS IDs **************** */
-		for (const tag of oldTags) { // setting getExistingTagsQuery to send the query
-			count != oldTags.length ? getExistingTagsQuery += ("'" + tag + "', ") : getExistingTagsQuery += ("'" + tag + "')")
-			count++
-		}
-		console.log('get existing tags:' + getExistingTagsQuery)
-		result = await queryPromise(getExistingTagsQuery)
-		console.log(result)
-		if ((newTags.length - result.length) > 0) { // if there are some new tags to add to the DB
-			const newTagsLength = newTags.length - result.length
-			let tagExists = false
-			let firstAddedTagId
-			count = 1
-			for (let tag of newTags) { // setting newTagsQuery and tagsIds
-				for (let existingTag of result) if (existingTag.value == tag) tagExists = true
-				if (!tagExists) {
-					count != newTagsLength ? insertNewTagsQuery += ("('" + tag + "'), ") : insertNewTagsQuery += ("('" + tag + "')")
-					count++
-				}
-				tagExists = false
-			}
-			console.log('insert these new tags:' + insertNewTagsQuery)
-			result = await queryPromise(insertNewTagsQuery)
-			firstAddedTagId = result.insertId
-			count = 0
-			while (count < newTagsLength) {
-				newTagsIDs.push(firstAddedTagId + count)
+const getNewTagsIDs = async (newTags) => {
+	for (const tag of newTags) { // setting getExistingTagsQuery to send the query
+		count != newTags.length ? getExistingTagsQuery += ("'" + tag + "', ") : getExistingTagsQuery += ("'" + tag + "')")
+		count++
+	}
+	console.log('get existing tags:' + getExistingTagsQuery)
+	existingTags = await queryPromise(getExistingTagsQuery)
+	console.log('existing tags: ' + existingTags)
+	if ((newTags.length - existingTags.length) > 0) { // if there are some new tags to add to the DB
+		const newTagsLength = newTags.length - existingTags.length
+		let tagExists = false
+		let firstAddedTagId
+		count = 1
+		for (let tag of newTags) { // setting newTagsQuery and tagsIds
+			for (let existingTag of existingTags) if (existingTag.value == tag) tagExists = true
+			if (!tagExists) {
+				count != newTagsLength ? insertNewTagsQuery += ("('" + tag + "'), ") : insertNewTagsQuery += ("('" + tag + "')")
 				count++
 			}
-			console.log('new tags IDs:' + newTagsIDs)
+			tagExists = false
 		}
+		console.log('insert these new tags:' + insertNewTagsQuery)
+		result = await queryPromise(insertNewTagsQuery)
+		firstAddedTagId = result.insertId
+		count = 0
+		while (count < newTagsLength) {
+			newTagsIDs.push(firstAddedTagId + count)
+			count++
+		}
+	}
+	for (count = 0 ; count < existingTags.length ; count++)
+		newTagsIDs.push(existingTags[count].id)
+	console.log('new tags ids' + newTagsIDs)
+}
 
-
-
-
-
-		/****************************************************** */
-
+router.post('/', validateToken, confirmIdentityWithPassword, isAccountComplete, async (req, res) => {
+	try {
+		const { newFirstname, newLastname, newUsername, newEmail, newPassword, newBirthday, newGender, newSexualPreferences, newBiography, oldTags, newTags} = req.body
+		if (newTags != null && oldTags != null) {
+			await getOldTagsIDs(oldTags)
+			await getNewTagsIDs(newTags)
+		}
 
 		// result = await queryPromise(
 		// 	"UPDATE usersTags SET " + 
 		// 		"tagId = 5 WHERE uid = 1 AND tagId = oldID"
 		// )
-	
+
 		// result = await queryPromise(
 		// 	"UPDATE users SET " +
 		// 		(newFirstname != null ? "firstname = ? " : "") +
