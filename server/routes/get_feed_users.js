ef12937f39646c612d4782755691b2ca55f86c25
Anas EL BOUDALI <anel-bou@student.1337.ma>
2022-07-17 01:39:22 +0100

    handling blocks in get_feed_users --in prog

--- server/routes/get_feed_users.js
+++ server/routes/get_feed_users.js
@@ -7,22 +7,38 @@ const util = require('util')
 const queryPromise = util.promisify(dbController.query.bind(dbController))	
 
 router.get('/', validateToken, isAccountComplete, async (req, res) => {
-	const { ageMin, ageMax, distance, ratingMin, ratingMax, sortedCommonTagsIDs, sort, pageNb, userPerPage } = req.body
+	const { ageMin, ageMax, distance, ratingMin, ratingMax, commonTagsIDs, sort, pageNb, userPerPage } = req.body
 	var result = await queryPromise(
-		`SELECT users.id, firstname, lastname, username, email, city, gender, ` +
-				`sexualPreferences, biography, longtitude, latitude, fameRating, ` +
-				`GROUP_CONCAT(images.image) AS userImages ` +
-			`FROM users \n` + 
-			`LEFT JOIN images ON users.id = images.uid \n` +
-			`WHERE TIMESTAMPDIFF(YEAR, birthday, CURDATE()) BETWEEN ${ageMin} AND ${ageMax} \n` +
-			`AND fameRating BETWEEN ${ratingMin} AND ${ratingMax} \n` + 
-			// distance
-			// unblocked ppl
-			// unmatched ppl
-			`GROUP BY users.id \n` +
-			`ORDER BY LENGTH(GROUP_CONCAT(images.image)) ASC \n` + 
-			`LIMIT ${userPerPage} OFFSET ${(pageNb - 1) * userPerPage} ` + 
-			``
+		`SELECT users.id, fameRating, ` +
+			`firstname, lastname, username, email, city, gender, sexualPreferences, biography, longtitude, latitude,  ` +
+			`GROUP_CONCAT(DISTINCT images.image ORDER BY images.image) AS images, \n` +
+			`(SELECT COUNT(images.image) FROM images WHERE images.uid = users.id) AS imagesCount, ` +
+			`(SELECT count(usersTags.uid) FROM usersTags WHERE usersTags.tagId \n`+
+				`IN ('1', '2', 3) \n`+
+				`AND usersTags.uid = users.id \n`+
+				`GROUP BY usersTags.uid) AS commonTagsCount \n` +
+		`FROM users \n` + 
+		
+		`LEFT JOIN images ON users.id = images.uid \n` +
+		`LEFT JOIN usersTags ON users.id = usersTags.uid \n` +
+		`LEFT JOIN blocks ON (users.id = blocks.uid OR users.id = blocks.blockedID) \n` +
+		// `LEFT JOIN matchedUsers ON (users.id = matchedUsers.uid1 OR users.id = matchedUsers.uid2) \n` +
+
+		`WHERE users.id != ${req.user.id} \n` +
+		`AND TIMESTAMPDIFF(YEAR, birthday, CURDATE()) BETWEEN ${ageMin} AND ${ageMax} \n` +
+		`AND fameRating BETWEEN ${ratingMin} AND ${ratingMax} \n` + 
+		`AND ((users.id != blocks.blockedID AND users.id != blocks.uid) OR blocks.uid IS NULL) \n` +
+		// `AND ((users.id != matchedUsers.uid1 AND users.id != matchedUsers.uid2) OR matchedUsers.uid1 IS NULL) \n` +
+
+		// distance
+		// unmatched ppl
+		// handle sexualPreferences
+		`GROUP BY users.id \n` +
+		`ORDER BY users.fameRating DESC, ` +
+			`commonTagsCount DESC, ` +
+			`imagesCount DESC \n` + 
+		`LIMIT ${userPerPage} OFFSET ${(pageNb - 1) * userPerPage} ` + 
+		``
 	)
 	console.log(result)
 	console.log("length=" + result.length)
